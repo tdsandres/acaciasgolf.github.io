9bd163641f39ad019e49eaca2b6dbc92a20567ba
zarate10 <zaratelautaro999@gmail.com>
2022-08-13 00:17:10 -0300

    ultimo commit de la noche

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,15 +16,20 @@ window.onscroll = function() {
 // comentarios 
 const $commentDescrip = $('.comentario-description')
 const $commentName = $('.comentario-nombre')
+const $commentWrapper = $('.box-comment')
 
 let listComment = [
     {
-        "nombre": "odfgkdfgkofgdkodfg",
-        "comentario": "oofdgkodfgkodfgkogdfokdgfokdfg"
+        "nombre": "Myriam Sandobal",
+        "comentario": "Muy lindo lugar. Muy recomendable y excelente atención de Lorena la dueña"
     }, 
     {
-        "nombre": "fgd09ihfgi09495",
-        "comentario": "fdghfog94639rhmidfhfg"
+        "nombre": "Karina",
+        "comentario": "Muy lindo lugar para disfrutar. Muy buena la atención de su dueña y empleados. Recomendable!"
+    },
+    {
+        "nombre": "Ainelen Berrettoni", 
+        "comentario": "Muy lindo lugar. Una tranquilidad, un paisaje hermoso. La gente muy tranquila. Todo muy limpio. La verdad súper recomendado para los que quieran pasar un buen momento. Impecable"
     }
 ]
 
@@ -38,15 +43,40 @@ document.addEventListener('DOMContentLoaded', () => {
      
         let commentRan = Math.round(Math.random(0, listComment.length)); 
 
-        $commentDescrip.innerHTML = listComment[numComment]["comentario"]
-        $commentName.innerHTML = listComment[numComment]["nombre"]
-        
         if (commentRan !== numComment){
-            numComment = commentRan;
+            numComment = commentRan
+
+            $commentDescrip.style.animation = 'left 1s'
+            $commentName.style.animation = 'left 1s'
+            $('.stars').style.animation = 'left 1s'
+            setTimeout(() => {
+    
+                $commentDescrip.innerHTML = listComment[numComment]["comentario"]
+                $commentName.innerHTML = listComment[numComment]["nombre"]
+                $commentDescrip.style.animation = 'right 1s'
+                $commentName.style.animation = 'right 1s'
+                $('.stars').style.animation = 'right 1s'
+    
+            }, 1000)
+        } else {
+            numComment = Math.round(Math.random(0, listComment.length)); 
         }
 
-        
-
-    }, 2000)
+    }, 3500)
 
 });
+
+// servicios btns {}
+
+const $more = $('#more'); 
+const $lbMore = document.querySelectorAll('#labelMore'); 
+
+$lbMore.forEach((btn) => {
+    btn.addEventListener('click', (el) => {
+        if (el.target.firstElementChild.innerHTML == '+'){
+            el.target.firstElementChild.innerHTML = '—'
+        } else {
+            el.target.firstElementChild.innerHTML = '+'
+        }
+    })
+})
